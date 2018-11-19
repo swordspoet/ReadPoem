061bb004abbf23a6d735e68da0512eed35a89917
libin <libin.xtu@foxmail.com>
2018-11-19 19:21:59 +0800

    TTS功能转移至客户端

--- client.py
+++ client.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 
 from socket import *
+from voice import Voice
+from player import Player
+
 
 host = 'localhost'
 port = 21765
@@ -10,15 +13,24 @@ address = (host, port)
 
 cli_sock = socket(AF_INET, SOCK_STREAM)
 cli_sock.connect(address)
+# 阿里云语音合成配置（秘钥可以找我索取）
+secret_id = ''
+secret_key = ''
+auth = Voice(secret_id, secret_key)
+player = Player()
+
 
 while 1:
     query = input("念诗一首: ")
     if not query:
         break
     cli_sock.send(query.encode())
-    data = cli_sock.recv(buff_size).decode()
+    data = cli_sock.recv(buff_size).decode()  # 客户端接收来自服务器的消息
+    print(data)
+
+    auth.save_voice(data.encode('utf8'), 'test')  # 将服务器返回的诗句合成为MP3文件
+    player.play_voice_by('test')                  # 调用系统播放器播放MP3文件
     if not data:
         break
-    print(data)
 
 cli_sock.close()
--- server.py
+++ server.py
@@ -3,39 +3,32 @@
 
 from socket import *
 from poetry_request import poetry_query
-from voice import Voice
-from player import Player
 
 
 host = 'localhost'
 port = 21765
 buff_size = 1024
-address = (host, port)
+address = (host, port)                      # 主机-地址对
 
-server_sock = socket(AF_INET, SOCK_STREAM)
-server_sock.bind(address)
-server_sock.listen(5)
-
-secret_id = 'LTAIsxMZyCpb7QFN'
-secret_key = 'uIRdXxLS4UyMbLY1uk5N3CI4SLl7AV'
-auth = Voice(secret_id, secret_key)
-player = Player()
+server_sock = socket(AF_INET, SOCK_STREAM)  # 创建服务器套接字
+server_sock.bind(address)                   # 套接字与主机-地址对绑定
+server_sock.listen(5)                       # 监听连接
 
 
-while 1:
-    cli_sock, addr = server_sock.accept()
+while 1:      # 服务器无线循环
+    cli_sock, addr = server_sock.accept()         # 接受客户端连接，开启单线程服务器
 
-    while 1:
-        query = cli_sock.recv(buff_size).decode()
+    while 1:  # 通信无限循环
+        query = cli_sock.recv(buff_size).decode()  # 接受客户端消息
         if not query:
             break
         query = poetry_query()
-        print(query.get('result').get('content'))
+        print(query.get('result').get('content'))  # 从API返回结果中提取诗句内容并发送至客户端
         content = query.get('result').get('content')
         cli_sock.send(bytes(content.encode('utf8')))
 
-        auth.save_voice(content.encode('utf8'), 'test')
-        player.play_voice_by('test')
+        # auth.save_voice(content.encode('utf8'), 'test')
+        # player.play_voice_by('test')
 
     cli_sock.close()
 server_sock.close()
